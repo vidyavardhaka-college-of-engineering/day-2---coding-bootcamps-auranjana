11f9b4a5a1676af32174c1a4f2d0539063ae8cb5
ranjanaau <>
2021-05-25 12:04:50 +0000

    swapping the values of a and b

--- Program_5.c
+++ Program_5.c
@@ -10,7 +10,11 @@
 int main()
 {
     int a, b, temp;
+    printf("Enter the values of a and b\n");
     scanf("%d%d", &a, &b);
+    temp=a;
+    a=b;
+    b=temp;
 
     //Write your code here
     
